6ec35c6891ca0f20fcdde4aa5dc2689a99260fed
Dmitriy Bublienko <d.bublienko@gmail.com>
2025-06-05 13:46:59 +0300

    Change home page from index page to route

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,9 @@
 import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
+import { HOME_PAGE, PRESALE_PAGE } from "./utils/constants";
 
 export default [
     layout("routes/pages/layout.tsx", [
-        index("routes/pages/home.tsx"),
-        route("/presale", "routes/pages/presale/index.tsx"),
+        route(HOME_PAGE, "routes/pages/home.tsx"),
+        route(PRESALE_PAGE, "routes/pages/presale/index.tsx"),
     ]),
 ] satisfies RouteConfig;
