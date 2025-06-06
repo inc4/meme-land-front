521a64804d0dd244572687ec802fa035ef7c9712
Dmitriy Bublienko <d.bublienko@gmail.com>
2025-06-06 11:20:17 +0300

    Add markup for connect wallet section

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,9 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 import { HOME_PAGE, PRESALE_PAGE } from "./utils/constants";
 
 export default [
+    layout("routes/pages/login/layout.tsx", [
+        index("routes/pages/login/index.tsx"),
+    ]),
     layout("routes/pages/layout.tsx", [
         route(HOME_PAGE, "routes/pages/home.tsx"),
         route(PRESALE_PAGE, "routes/pages/presale/index.tsx"),
---
+++ app/routes/pages/login/layout.tsx
@@ -0,0 +1,58 @@
+import { Outlet, Link } from "react-router";
+import clsx from "clsx";
+
+import Telegram from "~/components/Icons/Telegram";
+import X from "~/components/Icons/X";
+import logoIcon from "~/assets/svg/logo.svg";
+
+import { PRIVACY_NOTICE_PAGE, TERMS_OF_USE_PAGE } from "~/utils/constants";
+import getConfig from "~/config";
+
+const { X_LINK, TG_LINK } = getConfig();
+
+const LoginLayout = () => {
+  return (
+    <div className={clsx(
+      "flex flex-col justify-between h-full pt-[45px] pb-[28px] px-[16px] bg-gray-900",
+      "lg:pt-[85px] lg:pb-[60px] lg:px-[40px]"
+    )}>
+
+      <header>
+        <img className="m-auto" src={logoIcon} alt="Logo" />
+      </header>
+
+      <div className="grow">
+        <Outlet />
+      </div>
+
+      <footer className="flex items-center gap-[8px] m-auto">
+
+        <Link
+          to={PRIVACY_NOTICE_PAGE}
+          className="pr-[8px] text-beige-100 text-[14px] border-r-[1px] border-beige-600 hover:text-primary"
+        >
+          Privacy Policy
+        </Link>
+
+        <Link
+          to={TERMS_OF_USE_PAGE}
+          className="pr-[8px] text-beige-100 text-[14px] border-r-[1px] border-beige-600 hover:text-primary"
+        >
+          Terms & Condition
+        </Link>
+
+        <Link to={X_LINK} className="hover:[&_path]:fill-primary">
+          <X className="" />
+        </Link>
+
+        <Link to={TG_LINK} className="hover:[&_path]:fill-primary">
+          <Telegram />
+        </Link>
+
+      </footer>
+
+    </div>
+  )
+};
+
+export default LoginLayout;
