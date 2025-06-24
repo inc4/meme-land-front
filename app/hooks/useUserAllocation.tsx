adeedd338716285a920d4df91cfefa48c11c9144
Dmitriy Bublienko <d.bublienko@gmail.com>
2025-06-24 10:58:08 +0300

    Add useUserAllocation hook

---
+++ app/hooks/useUserAllocation.tsx
@@ -0,0 +1,54 @@
+import useSWR from "swr";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { Program, web3 } from "@coral-xyz/anchor";
+
+import useAnchorProvider from "./useAnchorProvider";
+import useCampaign from "./useCampaign";
+import getPdas from "~/utils/getPdas";
+import idl from '~/idl/mem_land.json'
+
+const useUserAllocation = (campaignId: string) => {
+  const { publicKey } = useWallet();
+  const provider = useAnchorProvider();
+  const { data: apiCampaign } = useCampaign(campaignId);
+  const { tokenName, tokenSymbol } = apiCampaign || {};
+
+  const fetcher = async (name: string, symbol: string, publicKey: web3.PublicKey) => {
+    const program = new Program(idl, provider);
+
+    const { campaignPda } = getPdas(name, symbol, program.programId, publicKey);
+
+    const [participantDataPda] = web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("participant_data"),
+        campaignPda.toBuffer(),
+        publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    const accountInfo = await provider.connection.getAccountInfo(participantDataPda);
+
+    if (accountInfo) {
+      const campaignStatsData = program.coder.accounts.decode(
+        "participantData",
+        accountInfo.data,
+      );
+
+      return campaignStatsData.amount as number;
+    }
+  };
+
+  return useSWR(
+    publicKey && tokenName && tokenSymbol ? {
+      key: 'allocation',
+      publicKey,
+      tokenName,
+      tokenSymbol,
+    } : null,
+    ({ tokenName, tokenSymbol, publicKey }) => fetcher(tokenName, tokenSymbol, publicKey),
+    { refreshInterval: 2000 },
+  )
+};
+
+export default useUserAllocation;
