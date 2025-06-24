2e5cfeeb3a935f250ad5a78f76e530272299b276
Dmitriy Bublienko <d.bublienko@gmail.com>
2025-06-24 17:41:59 +0300

    Add top 3 results in Leaderboard page, display ClaimModal

--- app/components/ClaimModal/index.tsx
+++ app/components/ClaimModal/index.tsx
@@ -1,8 +1,35 @@
+import { useWallet } from "@solana/wallet-adapter-react";
 import Modal from "~/components/Modal";
 import CustomInput from "~/components/CustomInput";
 import CustomButton from "~/components/CustomButton";
+import useClaim from "~/hooks/useClaim";
+import { formatPinataUrl } from "~/utils/formatPinataUrl";
+import type { TCampaign } from "~/types";
 
-const ClaimModal = ({isOpen, onClose}: {isOpen: boolean, onClose: () => void}) => {
+type TProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  campaign: TCampaign;
+  userAllocation: number;
+}
+
+const ClaimModal = ({ isOpen, onClose, campaign, userAllocation }: TProps) => {
+  const { tokenSymbol, tokenName, tokenImage, campaignId } = campaign;
+  const { isMutating, trigger: claim } = useClaim(campaignId);
+  const { publicKey } = useWallet();
+
+  const handleClaim = async () => {
+    if (!tokenName || !tokenSymbol || !publicKey) return;
+
+    try {
+      await claim({ name: tokenName, symbol: tokenSymbol, publicKey });
+      onClose();
+    } catch (e) {
+      console.log(e, 'Claim error');
+    }
+
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <div>
@@ -10,21 +37,26 @@ const ClaimModal = ({isOpen, onClose}: {isOpen: boolean, onClose: () => void}) =
         <div className="flex flex-col gap-[10px] my-8">
           <CustomInput
             label="You Invest"
-            value='22'
-            tokenName="SOL"
+            value={userAllocation.toString()}
+            tokenName={tokenSymbol}
+            tokenImage={formatPinataUrl(tokenImage)}
+            disabled
           />
         </div>
         <CustomButton
           customStyles="!text-body-l"
+          handleClick={handleClaim}
+          disabled={isMutating}
         >
-          Claim & Sell
+          Claim & Hold
         </CustomButton>
         <CustomButton
           variant="linear"
           customStyles="!text-body-l !border-[1px] !border-[#697586] mt-3"
           handleClick={onClose}
+          disabled={isMutating}
         >
-          Claim & Hold
+          Close
         </CustomButton>
       </div>
     </Modal>
--- app/components/CustomInput/index.tsx
+++ app/components/CustomInput/index.tsx
@@ -4,8 +4,10 @@ type CustomInputProps = {
   label: string;
   value: string;
   tokenName: string;
+  tokenImage?: string;
   onChange?: (value: string) => void;
   balance?: string;
+  disabled?: boolean;
 }
 
 const CustomInput = ({
@@ -13,6 +15,8 @@ const CustomInput = ({
   value,
   onChange = () => {},
   tokenName,
+  tokenImage = solanaLogo,
+  disabled = false,
   balance
 }: CustomInputProps) => {
 
@@ -44,12 +48,13 @@ const CustomInput = ({
         <input
           type="text"
           value={value}
+          disabled={disabled}
           onChange={(e) => handleInput(e.target.value)}
           className="font-mono text-h2 font-medium"
         />
         <div className="font-semibold ml-auto text-body-m py-2 px-3 h-[38px] flex items-center gap-2 bg-[#FFFFFF1F] rounded-xl">
           <span>{tokenName}</span>
-          <img src={solanaLogo} alt="logo"/>
+          <img src={tokenImage} className="w-[24px] h-auto object-contain" alt="logo"/>
         </div>
       </div>
       <div className="flex justify-between">
