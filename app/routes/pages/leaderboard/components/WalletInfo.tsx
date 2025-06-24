1c1302e12f5b831f55af1970cfabe4ebaa1f0bfe
Dmitriy Bublienko <d.bublienko@gmail.com>
2025-06-24 18:40:32 +0300

    Add dynamic width for WalletInfo's progress bar

--- app/routes/pages/leaderboard/components/WalletInfo.tsx
+++ app/routes/pages/leaderboard/components/WalletInfo.tsx
@@ -9,16 +9,20 @@ const WalletInfo = ({ campaignStats, userGroup }: TProps) => {
   const { walletsClaimed, totalParticipants } = campaignStats;
   const walletsWaiting = +totalParticipants - +walletsClaimed;
 
+  const widthPercentage = (+walletsClaimed * 100) / +totalParticipants;
+
   return (
     <div className="p-[12px] pt-[18px] bg-background border-[2px] border-gray-700 rounded-[12px]">
       <div className="flex flex-col items-center mb-[21px]">
         <span className="text-body-s text-beige-600 uppercase font-semibold">Current Wallet #</span>
         <span className="text-h2 text-primary font-medium font-mono">{userGroup || '---'}</span>
       </div>
-      
 
       <div className="w-full p-[2px] mb-[12px] bg-night rounded-[8px]">
-        <div className="w-1/3 bg-primary h-[20px] rounded-[4px] [box-shadow:_0px_0px_8px_0px_var(--color-primary)_inset;]"/>
+        <div
+          style={{ width: `${widthPercentage}%` }}
+          className="bg-primary h-[20px] rounded-[4px] [box-shadow:_0px_0px_8px_0px_var(--color-primary)_inset;]"
+        />
       </div>
 
       <div className="flex items-center gap-[10px] p-[12px] bg-beige-800">
