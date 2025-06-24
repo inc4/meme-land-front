3598971bcaf0c14f6db40f270ab8cc294e9e5f85
d.voronin <echelon1109@gmail.com>
2025-06-24 18:42:10 +0300

    layout fix

--- app/components/PresaleBlock/index.tsx
+++ app/components/PresaleBlock/index.tsx
@@ -99,21 +99,28 @@ const PresaleBlock = ({homePage, isLoading, campaign}:{homePage?:boolean, isLoad
         <div className="rounded-[14px] animate-pulse bg-neutral-900" />
       ) : campaign && (
         <div className="bg-[#0F1113] rounded-[14px] p-5">
-          <div className="relative">
-            <img src={formatPinataUrl(campaign.projectCoverImage)} alt="token" className="w-full h-[318px] object-cover rounded-[11px]"/>
-            <div style={{backgroundImage: `url("${bgFigure}"`}} className="absolute -top-[18px] bg-contain right-4 px-[33px] pt-[11px] pb-[9px] bg-no-repeat flex gap-1 items-stretch">
-              <div className="text-body-s bg-[#ED4646] text-white font-black uppercase rounded-[95px] h-[27px] px-[14px] flex items-center -mt-1">
-                live
-              </div>
-              <div className="font-mono border-[1px] border-[#FFFFFF14] rounded-[95px] text-body-s flex items-center pl-5 pr-[10px] -mt-1 before:w-[6px] before:h-[6px] before:rounded-full before:bg-[#12F287] before:left-[6px] before:absolute relative">
-                {campaignStatsData?.totalParticipants.toNumber()} Participant{campaignStatsData?.totalParticipants.toNumber() > 1 && 's'}
+          {campaign.currentStatus === 'presaleOpened' && (
+            <div className="relative">
+              <img src={formatPinataUrl(campaign.projectCoverImage)} alt="token"
+                   className="w-full h-[318px] object-cover rounded-[11px]"/>
+              <div style={{backgroundImage: `url("${bgFigure}"`}}
+                   className="absolute -top-[18px] bg-contain right-4 px-[33px] pt-[11px] pb-[9px] bg-no-repeat flex gap-1 items-stretch">
+                <div
+                  className="text-body-s bg-[#ED4646] text-white font-black uppercase rounded-[95px] h-[27px] px-[14px] flex items-center -mt-1">
+                  live
+                </div>
+                <div
+                  className="font-mono border-[1px] border-[#FFFFFF14] rounded-[95px] text-body-s flex items-center pl-5 pr-[10px] -mt-1 before:w-[6px] before:h-[6px] before:rounded-full before:bg-[#12F287] before:left-[6px] before:absolute relative">
+                  {campaignStatsData?.totalParticipants.toNumber()} Participant{campaignStatsData?.totalParticipants.toNumber() > 1 && 's'}
+                </div>
               </div>
             </div>
-          </div>
+          )}
           <div className="flex flex-col mt-5">
             <div className="flex flex-col gap-6 mb-4 lg:flex-row lg:justify-between">
               <div className="flex gap-[10px] items-center">
-                <img src={formatPinataUrl(campaign.tokenImage)} alt="logo" className="w-[62px] h-[62px] object-cover rounded-[10px]"/>
+                <img src={formatPinataUrl(campaign.tokenImage)} alt="logo"
+                     className="w-[62px] h-[62px] object-cover rounded-[10px]"/>
                 <div className="flex flex-col gap-[2px]">
                   <span className="text-white font-bold text-[24px]">{campaign.projectName}</span>
                   <span className="opacity-60 text-white text-body-m">{campaign.shortDescription1}</span>
