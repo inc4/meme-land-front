3574fd98123cdaa6f080d0709d61579461abee3d
d.voronin <echelon1109@gmail.com>
2025-07-11 17:59:18 +0300

    arrow fix

--- app/components/Welcome/components/HowItWorks.tsx
+++ app/components/Welcome/components/HowItWorks.tsx
@@ -80,7 +80,9 @@ const HowItWorks = () => {
             <div className="px-1">
               <div className="flex flex-col relative bg-[#0F1113] rounded-[14px] p-5 min-h-[202px] mr-6">
                 <img className="w-12 h-12" src={el.img} alt="icon"/>
-                <img src={arrowIcon} alt="arrow" className="absolute top-9 right-5"/>
+                {i < cards.length - 1 && (
+                  <img src={arrowIcon} alt="arrow" className="absolute top-9 right-5"/>
+                )}
                 <span className="mt-5 mb-2 font-bold text-[20px]">{el.title}</span>
                 <span className="text-body-m opacity-60">{el.subtitle}</span>
               </div>
