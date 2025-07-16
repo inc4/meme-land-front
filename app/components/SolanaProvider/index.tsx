5035677bdfac0ed1b36fe31be6442b09b55eb4ed
Dmitriy Bublienko <d.bublienko@gmail.com>
2025-07-16 14:40:29 +0300

    Replace clusterApiUrl with env rpc url in SolanaProvider

--- app/components/SolanaProvider/index.tsx
+++ app/components/SolanaProvider/index.tsx
@@ -2,14 +2,10 @@ import { useMemo, type ReactNode } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
-import getConfig from '~/config';
 import '@solana/wallet-adapter-react-ui/styles.css';
 
-const { NETWORK_ID } = getConfig();
-
 const SolanaProvider = ({ children }: { children: ReactNode }) => {
-  const endpoint = useMemo(() => clusterApiUrl(NETWORK_ID), []);
+  const endpoint = useMemo(() => import.meta.env.VITE_RPC_URL, []);
 
   const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
